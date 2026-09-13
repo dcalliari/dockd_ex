81f6795775308c76ff317da78ccf88ea45da9147
daniel calliari <dbcalliari@gmail.com>
2026-09-13 20:54:05 -0300

    fix(compose): isolate proxy deployment from host ports

--- config/dev.exs
+++ config/dev.exs
@@ -23,10 +23,10 @@ config :dockd, Dockd.Repo,
 # The watchers configuration can be used to run external
 # watchers to your application. For example, we can use it
 # to bundle .js and .css sources.
+bind_ip = if System.get_env("DOCKD_BIND_ALL") in ~w(true 1), do: {0, 0, 0, 0}, else: {127, 0, 0, 1}
+
 config :dockd, DockdWeb.Endpoint,
-  # Binding to loopback ipv4 address prevents access from other machines.
-  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}],
+  http: [ip: bind_ip],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
