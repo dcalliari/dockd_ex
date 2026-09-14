3d0d591ecfc691524474eb8b36d45443eb379f4d
daniel calliari <dbcalliari@gmail.com>
2026-09-14 09:35:57 -0300

    fix(web): remove duplicate component import

--- lib/dockd_web.ex
+++ lib/dockd_web.ex
@@ -87,7 +87,6 @@ defmodule DockdWeb do
       # Core UI components
       import DockdWeb.CoreComponents
       import DockdWeb.DockdComponents
-      import DockdWeb.DockdComponents
 
       # Common modules used in templates
       alias DockdWeb.Layouts
