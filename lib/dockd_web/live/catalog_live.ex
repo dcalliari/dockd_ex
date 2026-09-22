e78c9cc21841b4546e2793a2421dd3b2dcf61387
daniel calliari <dbcalliari@gmail.com>
2026-09-22 11:25:29 -0300

    feat(ui): make catalog and library personal

--- lib/dockd_web/live/catalog_live.ex
+++ lib/dockd_web/live/catalog_live.ex
@@ -7,14 +7,28 @@ defmodule DockdWeb.CatalogLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, page_title: "Catálogo", games: Catalog.list_games())}
+    user = Accounts.default_owner()
+    library_game_ids = user |> Library.list_entries() |> MapSet.new(& &1.game_id)
+
+    {:ok,
+     assign(socket,
+       page_title: "Catálogo",
+       games: Catalog.list_games(),
+       library_game_ids: library_game_ids
+     )}
   end
 
   @impl true
   def handle_event("add_to_library", %{"id" => id}, socket) do
     case Library.create_entry(Accounts.default_owner(), %{game_id: id}) do
-      {:ok, _entry} -> {:noreply, put_flash(socket, :info, "Jogo adicionado à biblioteca.")}
-      {:error, _changeset} -> {:noreply, put_flash(socket, :error, "Jogo já está na biblioteca.")}
+      {:ok, _entry} ->
+        {:noreply,
+         socket
+         |> assign(:library_game_ids, MapSet.put(socket.assigns.library_game_ids, id))
+         |> put_flash(:info, "Jogo adicionado à biblioteca.")}
+
+      {:error, _changeset} ->
+        {:noreply, put_flash(socket, :error, "Jogo já está na biblioteca.")}
     end
   end
 end
--- lib/dockd_web/live/library_live.ex
+++ lib/dockd_web/live/library_live.ex
@@ -17,14 +17,16 @@ defmodule DockdWeb.LibraryLive do
     do: {:noreply, load(socket, socket.assigns.user, params)}
 
   @impl true
-  def handle_event("filter", params, socket),
-    do: {:noreply, load(socket, socket.assigns.user, params)}
+  def handle_event("filter", params, socket) do
+    filters = Map.merge(socket.assigns.filters, normalize_params(params))
+    {:noreply, load(socket, socket.assigns.user, filters)}
+  end
 
   def handle_event("new_entry", %{"game_id" => game_id}, socket) do
     case Library.create_entry(socket.assigns.user, %{game_id: game_id}) do
       {:ok, _entry} ->
         {:noreply,
-         load(socket, socket.assigns.user, %{})
+         load(socket, socket.assigns.user, socket.assigns.filters)
          |> put_flash(:info, "Jogo adicionado à biblioteca.")}
 
       {:error, changeset} ->
@@ -37,13 +39,25 @@ defmodule DockdWeb.LibraryLive do
     {:noreply, assign(socket, editing: entry, form: to_form(Entry.changeset(entry, %{})))}
   end
 
+  def handle_event("close_editor", _params, socket), do: {:noreply, assign(socket, editing: nil)}
+
+  def handle_event("quick_state", %{"id" => id, "play_state" => state}, socket) do
+    entry = Library.get_entry!(socket.assigns.user, id)
+
+    case Library.update_entry(socket.assigns.user, entry, %{play_state: state}) do
+      {:ok, _entry} -> {:noreply, load(socket, socket.assigns.user, socket.assigns.filters)}
+      {:error, changeset} -> {:noreply, put_flash(socket, :error, inspect(changeset.errors))}
+    end
+  end
+
   def handle_event("save_entry", %{"entry" => attrs}, socket) do
     entry = socket.assigns.editing
 
     case Library.update_entry(socket.assigns.user, entry, attrs) do
       {:ok, _entry} ->
         {:noreply,
-         load(socket, socket.assigns.user, %{}) |> put_flash(:info, "Entrada atualizada.")}
+         load(socket, socket.assigns.user, socket.assigns.filters)
+         |> put_flash(:info, "Entrada atualizada.")}
 
       {:error, changeset} ->
         {:noreply, assign(socket, form: to_form(changeset))}
@@ -54,11 +68,8 @@ defmodule DockdWeb.LibraryLive do
     ownership = Library.get_ownership!(socket.assigns.user, id)
 
     case Library.delete_ownership(socket.assigns.user, ownership) do
-      {:ok, _} ->
-        {:noreply, load(socket, socket.assigns.user, %{})}
-
-      {:error, reason} ->
-        {:noreply, put_flash(socket, :error, "Não foi possível remover: #{inspect(reason)}")}
+      {:ok, _} -> {:noreply, load(socket, socket.assigns.user, socket.assigns.filters)}
+      {:error, reason} -> {:noreply, put_flash(socket, :error, "Não foi possível remover: #{inspect(reason)}")}
     end
   end
 
@@ -68,7 +79,8 @@ defmodule DockdWeb.LibraryLive do
     case Library.delete_entry(socket.assigns.user, entry) do
       {:ok, _} ->
         {:noreply,
-         load(socket, socket.assigns.user, %{}) |> put_flash(:info, "Entrada removida.")}
+         load(socket, socket.assigns.user, socket.assigns.filters)
+         |> put_flash(:info, "Entrada removida.")}
 
       {:error, reason} ->
         {:noreply, put_flash(socket, :error, "Não foi possível remover: #{inspect(reason)}")}
@@ -76,53 +88,68 @@ defmodule DockdWeb.LibraryLive do
   end
 
   defp load(socket, user, params) do
+    filters = normalize_params(params)
     all_entries = Library.list_entries(user)
     ownerships = Library.list_ownerships(user)
-    owned_game_ids = MapSet.new(ownerships, & &1.release.game_id)
-
-    filters =
-      params
-      |> Map.take(["search", "backlog", "play_state", "tab"])
-      |> Map.put_new("tab", "all")
-
-    entry_filters = if Map.has_key?(params, "tab"), do: filters, else: Map.delete(filters, "tab")
-    entries = Library.list_entries(user, entry_filters)
+    owned_game_ids = ownerships |> Enum.map(& &1.release.game_id) |> MapSet.new()
+    entries = filter_entries(all_entries, filters, owned_game_ids)
     games = Catalog.list_games()
+    entry_game_ids = MapSet.new(all_entries, & &1.game_id)
+    counts = counts(all_entries, owned_game_ids)
 
     assign(socket,
       page_title: "Biblioteca",
       user: user,
       entries: entries,
+      all_entries: all_entries,
       ownerships: ownerships,
+      orphan_ownerships: Enum.reject(ownerships, &MapSet.member?(entry_game_ids, &1.release.game_id)),
+      ownerships_by_game: Enum.group_by(ownerships, & &1.release.game_id),
+      owned_game_ids: owned_game_ids,
       games: games,
       editing: nil,
       form: to_form(Entry.changeset(%Entry{}, %{})),
       filters: filters,
-      tab_counts: tab_counts(all_entries, owned_game_ids),
-      groups: Enum.group_by(entries, &group_for/1)
+      counts: counts,
+      tab_counts: Map.put(counts, :all, counts.collection)
     )
   end
 
-  defp tab_counts(entries, owned_game_ids) do
+  defp normalize_params(params) do
+    nested = Map.get(params, "filters", %{})
+    params = Map.merge(Map.drop(params, ["filters"]), nested)
+    Map.put_new(params, "tab", "all")
+  end
+
+  defp filter_entries(entries, filters, owned_game_ids) do
+    search = filters |> Map.get("search", "") |> to_string() |> String.downcase()
+
+    Enum.filter(entries, fn entry ->
+      matches_search = search == "" or String.contains?(String.downcase(entry.game.title), search)
+
+      matches_tab =
+        case Map.get(filters, "tab") do
+          "playing" -> entry.play_state == :playing
+          "backlog" -> entry.backlog == :backlog
+          "want" -> entry.purchase_intent in [:want, :planned, :preordered]
+          "all" -> MapSet.member?(owned_game_ids, entry.game_id)
+          "finished" -> entry.play_state == :finished
+          _ -> true
+        end
+
+      matches_search and matches_tab
+    end)
+  end
+
+  defp counts(entries, owned_game_ids) do
     %{
       playing: Enum.count(entries, &(&1.play_state == :playing)),
       backlog: Enum.count(entries, &(&1.backlog == :backlog)),
       want: Enum.count(entries, &(&1.purchase_intent in [:want, :planned, :preordered])),
-      all: Enum.count(entries, &(&1.game_id in owned_game_ids)),
+      collection: MapSet.size(owned_game_ids),
       finished: Enum.count(entries, &(&1.play_state == :finished))
     }
   end
-
-  defp group_for(%{play_state: :playing}), do: "Jogando"
-  defp group_for(%{backlog: :backlog}), do: "Backlog"
-
-  defp group_for(%{purchase_intent: intent}) when intent in [:want, :planned, :preordered],
-    do: "Quero comprar"
-
-  defp group_for(%{play_state: state}) when state in [:finished, :abandoned],
-    do: "Terminados / abandonados"
-
-  defp group_for(_), do: "Outros"
 
   defp status_label(%{play_state: :playing}), do: "Jogando"
   defp status_label(%{play_state: :finished}), do: "Terminado"
@@ -132,4 +159,14 @@ defmodule DockdWeb.LibraryLive do
 
   defp platform_label(%{releases: [%{platform: platform} | _]}), do: enum_label(platform)
   defp platform_label(_), do: nil
+
+  defp ownership_label(ownerships_by_game, game_id) do
+    ownerships_by_game
+    |> Map.get(game_id, [])
+    |> List.first()
+    |> case do
+      nil -> nil
+      ownership -> enum_label(ownership.ownership_type)
+    end
+  end
 end
